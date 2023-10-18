d405bbc3722b842e4cc0c9eee78bacc99e6861a4
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-10-18 14:46:31 +0700

    baihaqi withdraw & top up 2

--- Systembank.java
+++ Systembank.java
@@ -9,9 +9,9 @@ public class Systembank {
        
         
         while (login) {
-            System.out.print("Input Username = ");
+            System.out.print("~ Input Username = ");
             String user = scan.next(); 
-            System.out.print("Input Password = ");
+            System.out.print("~ Input Password = ");
             int pass = scan.nextInt();
             if (user.equals("Majid") && pass == 123){
                 login = false;
@@ -36,9 +36,8 @@ public class Systembank {
             System.out.println("5. Balance Info");
             System.out.println("====================================");
             System.out.print("Choose the menu : ");
-            
             int pilihan = scan.nextInt();
-         
+            System.out.println("====================================");
             switch (pilihan) {
                 case 1:
                 System.out.println("Please select the withdrawal amount");
@@ -47,50 +46,76 @@ public class Systembank {
                 System.out.println("3. Rp 500.000");
                 System.out.println("4. Rp 1.000.000");
                 System.out.println("5. Other Amount");
-                System.out.print("Choose menu : ");
+                System.out.println("====================================");
+                System.out.print("Choose Nominal : ");
                 int witdrawalamount = scan.nextInt();
                     switch (witdrawalamount) {
                         case 1:
-                            System.out.println("=============================");
-                            System.out.println(" You Withdrawal : Rp 100.000  ");
-                            System.out.println("    Transaction Complete     ");
-                            System.out.println("         Thank You :)        ");
-                            System.out.println("=============================");
+                            System.out.println("====================================");
+                            System.out.println("    You Withdrawal : Rp 100.000  ");
+                            System.out.println("        Transaction Complete     ");
+                            System.out.println("            Thank You :)        ");
+                            System.out.println("====================================");
+                            menu = false;
                         break;
                         case 2:
-                            System.out.println("=============================");
-                            System.out.println(" You Withdrawal : Rp 250.000  ");
-                            System.out.println("    Transaction Complete     ");
-                            System.out.println("         Thank You :)        ");
-                            System.out.println("=============================");
+                            System.out.println("====================================");
+                            System.out.println("    You Withdrawal : Rp 250.000  ");
+                            System.out.println("        Transaction Complete     ");
+                            System.out.println("            Thank You :)        ");
+                            System.out.println("====================================");
+                            menu = false;
                         break;
                         case 3:
-                            System.out.println("=============================");
-                            System.out.println(" You Withdrawal : Rp 500.000  ");
-                            System.out.println("    Transaction Complete     ");
-                            System.out.println("         Thank You :)        ");
-                            System.out.println("=============================");
+                            System.out.println("====================================");
+                            System.out.println("    You Withdrawal : Rp 500.000  ");
+                            System.out.println("        Transaction Complete     ");
+                            System.out.println("            Thank You :)        ");
+                            System.out.println("====================================");
+                            menu = false;
                         break;
                         case 4:
-                            System.out.println("=============================");
-                            System.out.println(" You Withdrawal : Rp 1.000.000");
-                            System.out.println("    Transaction Complete     ");
-                            System.out.println("         Thank You :)        ");
-                            System.out.println("=============================");
+                            System.out.println("====================================");
+                            System.out.println("   You Withdrawal : Rp 1.000.000");
+                            System.out.println("        Transaction Complete     ");
+                            System.out.println("            Thank You :)        ");
+                            System.out.println("====================================");
+                            menu = false;
                         break;
                         case 5:
+                            System.out.println("Multiples Of Rp 50.000");
                             System.out.print("Type The Amount : ");
                             int typeamount = scan.nextInt();
-                            System.out.println("=============================");
-                            System.out.println(" You Withdrawal : Rp " + typeamount);
-                            System.out.println("    Transaction Complete     ");
-                            System.out.println("         Thank You :)        ");
-                            System.out.println("=============================");
+                            System.out.println("====================================");
+                            System.out.println("   You Withdrawal : Rp " + typeamount);
+                            System.out.println("        Transaction Complete     ");
+                            System.out.println("            Thank You :)        ");
+                            System.out.println("====================================");
+                            menu = false;
+                        break;
                 }
                     break;
                 case 2:
-                    
-                    break;
+                    System.out.println("Input Virtual Account : ");
+                    int virtualacc = scan.nextInt();
+                    System.out.println("Input Nominal : ");
+                    int inputnominal = scan.nextInt();
+                    System.out.println("=========================================================");
+                    System.out.println("You Will Topup To " + virtualacc + " Of Rp " + inputnominal );
+                    System.out.println("=========================================================");
+                    System.out.println("Continue To Proceed (y/n) : ");
+                    String choice = scan.next();
+                        if (choice.equalsIgnoreCase("y")) {
+                            System.out.println("Topup Rp " + inputnominal + " To " + virtualacc);
+                            System.out.println("    Done");
+                            System.out.println(" Thankyou :)");
+                            menu = false;
+                            
+                        
+                        }
+                        else 
+                            System.out.println("Okey");
+                            break;
                 case 3:
                     
                     break;
